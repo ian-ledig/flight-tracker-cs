79119b465dee12c098b0d933164c2d917f98adbc
Ian Ledig <ian.ledigfr@gmail.com>
2025-06-07 17:33:13 +0900

    editing port and basepath to match prod

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: '/flighttracker',
+  assetPrefix: '/flighttracker/',
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
