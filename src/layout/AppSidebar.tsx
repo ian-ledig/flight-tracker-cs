5ae258b02d37cb82fe45412da5f545561ae6d491
Ian Ledig <ian.ledigfr@gmail.com>
2025-06-09 11:05:25 +0900

    fix image path

--- src/layout/AppSidebar.tsx
+++ src/layout/AppSidebar.tsx
@@ -10,6 +10,9 @@ import {
   FlightIcon,
   HomeInfoIcon,
 } from '../icons/index';
+import nextConfig from '../../next.config';
+
+const basePath = nextConfig.basePath;
 
 type NavItem = {
   name: string;
@@ -249,14 +252,14 @@ const AppSidebar: React.FC = () => {
             <>
               <Image
                 className="dark:hidden"
-                src="/images/logo/logo.svg"
+                src={`${basePath}/images/logo/logo.svg`}
                 alt="Logo"
                 width={150}
                 height={40}
               />
               <Image
                 className="hidden dark:block"
-                src="/images/logo/logo-dark.svg"
+                src={`${basePath}/images/logo/logo-dark.svg`}
                 alt="Logo"
                 width={150}
                 height={40}
@@ -264,7 +267,7 @@ const AppSidebar: React.FC = () => {
             </>
           ) : (
             <Image
-              src="/images/logo/logo-icon.svg"
+              src={`${basePath}/images/logo/logo-icon.svg`}
               alt="Logo"
               width={32}
               height={32}
