cd3f34b47eb00a9518388b022da9789f38f70209
Ian <ian.ledigfr@gmail.com>
2025-06-10 17:22:25 +0900

    Update next.config.ts to add trailingslash

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   basePath: '/flight-tracker',
   assetPrefix: '/flight-tracker/',
+  trailingSlash: true,
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
